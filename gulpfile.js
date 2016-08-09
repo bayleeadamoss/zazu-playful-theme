a4d990c3578a76b4e6db46194b7c60950df0f4ff
Blaine Schmeisser <blainesch@gmail.com>
2016-08-09 08:56:45 -0700

    Use the inline image for zazu.

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,22 @@
-var gulp = require('gulp');
-var postcss = require('gulp-postcss');
-var nested = require('postcss-nested');
-var scss = require('postcss-scss');
-var colorFunction = require('postcss-color-function');
+var gulp = require('gulp')
+var postcss = require('gulp-postcss')
+var nested = require('postcss-nested')
+var scss = require('postcss-scss')
+var colorFunction = require('postcss-color-function')
 var vars = require('postcss-simple-vars')
+var postcssUrl = require('postcss-url')
 
 gulp.task('build', function () {
-    var processors = [
-      nested,
-      colorFunction(),
-      vars()
-    ];
-    return gulp.src('css/main.css')
-        .pipe(postcss(processors, {syntax: scss}))
-        .pipe(gulp.dest('dist'));
-});
+  var processors = [
+    nested,
+    colorFunction(),
+    vars(),
+    postcssUrl({
+      url: 'inline',
+      maxSize: 999,
+    }),
+  ]
+  return gulp.src('css/main.css')
+    .pipe(postcss(processors, {}))
+    .pipe(gulp.dest('dist'))
+})
