1dc70a675c871a405ff25d1fe9fc66547e97fad1
Blaine Schmeisser <blainesch@gmail.com>
2017-01-08 16:18:09 -0800

    Remove old config file.

--- zazu.js
+++
@@ -1,8 +0,0 @@
-// Deprecated use zazu.json instead
-module.exports = {
-  name: 'Playful Theme',
-  version: '1.0.0',
-  author: 'blainesch',
-  description: 'A basic playful theme for Zazu',
-  stylesheet: 'dist/main.css'
-}
